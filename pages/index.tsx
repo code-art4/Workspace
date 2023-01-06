b7277e4efb80cc986fb2f338838fb8ffa9534f1f
code-art4 <ojotriumph4@gmail.com>
2023-01-06 15:27:12 +0100

    enhancement: completed responsive for laptops

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { Nav, Workspace } from './components/Home';
+import { Nav, Workspace } from '../components/Home';
 
 export default function Home() {
   return (
